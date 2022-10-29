88fa79e70fba20fb728ef9e35b83eb037da434d2
intsuc <intsuc@gmail.com>
2022-10-29 18:34:14 +0900

    Zip datapack on distribution

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,41 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.20"
-    application
+  kotlin("jvm") version "1.7.20"
+  application
 }
 
 version = "0.1.0"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
-    testImplementation(kotlin("test"))
+  implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
+  testImplementation(kotlin("test"))
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+  kotlinOptions.jvmTarget = "17"
 }
 
 application {
-    mainClass.set("mcx.cli.MainKt")
+  mainClass.set("mcx.cli.MainKt")
+}
+
+tasks.register<Zip>("zipDatapack") {
+  archiveFileName.set("mcx.zip")
+  isPreserveFileTimestamps = false
+  isReproducibleFileOrder = true
+  from(layout.projectDirectory.dir("mcx.zip"))
+}
+
+tasks.withType<ProcessResources> {
+  dependsOn(tasks.getByName("zipDatapack"))
+  from(layout.buildDirectory.file("distributions/mcx.zip"))
 }
