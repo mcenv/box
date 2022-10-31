ba74213639eb8f43e905ad635f3c358207ee4308
intsuc <intsuc@gmail.com>
2022-10-31 20:47:47 +0900

    Add cache

--- src/main/kotlin/mcx/lsp/McxTextDocumentService.kt
+++ src/main/kotlin/mcx/lsp/McxTextDocumentService.kt
@@ -1,31 +1,37 @@
 package mcx.lsp
 
-import mcx.phase.*
+import mcx.phase.Cache
+import mcx.phase.Context
 import org.eclipse.lsp4j.*
 import org.eclipse.lsp4j.jsonrpc.CompletableFutures.computeAsync
 import org.eclipse.lsp4j.services.TextDocumentService
 import java.util.concurrent.CompletableFuture
-import mcx.ast.Core as C
 
 class McxTextDocumentService : TextDocumentService {
-  private val texts: HashMap<String, String> = hashMapOf()
+  private val cache: Cache = Cache()
 
   override fun didOpen(
     params: DidOpenTextDocumentParams,
   ) {
-    texts[params.textDocument.uri] = params.textDocument.text
+    cache.modifyDocument(
+      params.textDocument.uri,
+      params.textDocument.text,
+    )
   }
 
   override fun didChange(
     params: DidChangeTextDocumentParams,
   ) {
-    texts[params.textDocument.uri] = params.contentChanges.last().text
+    cache.modifyDocument(
+      params.textDocument.uri,
+      params.contentChanges.last().text,
+    )
   }
 
   override fun didClose(
     params: DidCloseTextDocumentParams,
   ) {
-    texts -= params.textDocument.uri
+    cache.closeDocument(params.textDocument.uri)
   }
 
   override fun didSave(
@@ -38,14 +44,10 @@ class McxTextDocumentService : TextDocumentService {
   ): CompletableFuture<DocumentDiagnosticReport> =
     computeAsync {
       val context = Context()
-      phase(
+      cache.fetchCore(
         context,
-        texts[params.textDocument.uri]!!,
+        params.textDocument.uri,
       )
       DocumentDiagnosticReport(RelatedFullDocumentDiagnosticReport(context.diagnostics))
     }
-
-  companion object {
-    private val phase: Phase<String, C.Root> = Parse..Elaborate
-  }
 }
