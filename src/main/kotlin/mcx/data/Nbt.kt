43afd42d3f6e147be58c761d0c611c2d78581409
intsuc <intsuc@gmail.com>
2023-04-03 23:28:48 +0900

    Bump kotlin to 1.8.20

--- src/main/kotlin/mcx/data/Nbt.kt
+++ src/main/kotlin/mcx/data/Nbt.kt
@@ -8,44 +8,34 @@ sealed interface Nbt {
   object End : Nbt
 
   @Serializable
-  @JvmInline
-  value class Byte(val value: kotlin.Byte) : Nbt
+  data class Byte(val value: kotlin.Byte) : Nbt
 
   @Serializable
-  @JvmInline
-  value class Short(val value: kotlin.Short) : Nbt
+  data class Short(val value: kotlin.Short) : Nbt
 
   @Serializable
-  @JvmInline
-  value class Int(val value: kotlin.Int) : Nbt
+  data class Int(val value: kotlin.Int) : Nbt
 
   @Serializable
-  @JvmInline
-  value class Long(val value: kotlin.Long) : Nbt
+  data class Long(val value: kotlin.Long) : Nbt
 
   @Serializable
-  @JvmInline
-  value class Float(val value: kotlin.Float) : Nbt
+  data class Float(val value: kotlin.Float) : Nbt
 
   @Serializable
-  @JvmInline
-  value class Double(val value: kotlin.Double) : Nbt
+  data class Double(val value: kotlin.Double) : Nbt
 
   @Serializable
-  @JvmInline
-  value class ByteArray(val elements: kotlin.collections.List<kotlin.Byte>) : Nbt
+  data class ByteArray(val elements: kotlin.collections.List<kotlin.Byte>) : Nbt
 
   @Serializable
-  @JvmInline
-  value class IntArray(val elements: kotlin.collections.List<kotlin.Int>) : Nbt
+  data class IntArray(val elements: kotlin.collections.List<kotlin.Int>) : Nbt
 
   @Serializable
-  @JvmInline
-  value class LongArray(val elements: kotlin.collections.List<kotlin.Long>) : Nbt
+  data class LongArray(val elements: kotlin.collections.List<kotlin.Long>) : Nbt
 
   @Serializable
-  @JvmInline
-  value class String(val value: kotlin.String) : Nbt
+  data class String(val value: kotlin.String) : Nbt
 
   @Serializable
   sealed interface List<T : Nbt> : Nbt {
@@ -57,55 +47,42 @@ sealed interface Nbt {
     }
 
     @Serializable
-    @JvmInline
-    value class Byte(override val elements: kotlin.collections.List<Nbt.Byte>) : Nbt.List<Nbt.Byte>
+    data class Byte(override val elements: kotlin.collections.List<Nbt.Byte>) : Nbt.List<Nbt.Byte>
 
     @Serializable
-    @JvmInline
-    value class Short(override val elements: kotlin.collections.List<Nbt.Short>) : Nbt.List<Nbt.Short>
+    data class Short(override val elements: kotlin.collections.List<Nbt.Short>) : Nbt.List<Nbt.Short>
 
     @Serializable
-    @JvmInline
-    value class Int(override val elements: kotlin.collections.List<Nbt.Int>) : Nbt.List<Nbt.Int>
+    data class Int(override val elements: kotlin.collections.List<Nbt.Int>) : Nbt.List<Nbt.Int>
 
     @Serializable
-    @JvmInline
-    value class Long(override val elements: kotlin.collections.List<Nbt.Long>) : Nbt.List<Nbt.Long>
+    data class Long(override val elements: kotlin.collections.List<Nbt.Long>) : Nbt.List<Nbt.Long>
 
     @Serializable
-    @JvmInline
-    value class Float(override val elements: kotlin.collections.List<Nbt.Float>) : Nbt.List<Nbt.Float>
+    data class Float(override val elements: kotlin.collections.List<Nbt.Float>) : Nbt.List<Nbt.Float>
 
     @Serializable
-    @JvmInline
-    value class Double(override val elements: kotlin.collections.List<Nbt.Double>) : Nbt.List<Nbt.Double>
+    data class Double(override val elements: kotlin.collections.List<Nbt.Double>) : Nbt.List<Nbt.Double>
 
     @Serializable
-    @JvmInline
-    value class ByteArray(override val elements: kotlin.collections.List<Nbt.ByteArray>) : Nbt.List<Nbt.ByteArray>
+    data class ByteArray(override val elements: kotlin.collections.List<Nbt.ByteArray>) : Nbt.List<Nbt.ByteArray>
 
     @Serializable
-    @JvmInline
-    value class IntArray(override val elements: kotlin.collections.List<Nbt.IntArray>) : Nbt.List<Nbt.IntArray>
+    data class IntArray(override val elements: kotlin.collections.List<Nbt.IntArray>) : Nbt.List<Nbt.IntArray>
 
     @Serializable
-    @JvmInline
-    value class LongArray(override val elements: kotlin.collections.List<Nbt.LongArray>) : Nbt.List<Nbt.LongArray>
+    data class LongArray(override val elements: kotlin.collections.List<Nbt.LongArray>) : Nbt.List<Nbt.LongArray>
 
     @Serializable
-    @JvmInline
-    value class String(override val elements: kotlin.collections.List<Nbt.String>) : Nbt.List<Nbt.String>
+    data class String(override val elements: kotlin.collections.List<Nbt.String>) : Nbt.List<Nbt.String>
 
     @Serializable
-    @JvmInline
-    value class List(override val elements: kotlin.collections.List<Nbt.List<*>>) : Nbt.List<Nbt.List<*>>
+    data class List(override val elements: kotlin.collections.List<Nbt.List<*>>) : Nbt.List<Nbt.List<*>>
 
     @Serializable
-    @JvmInline
-    value class Compound(override val elements: kotlin.collections.List<Nbt.Compound>) : Nbt.List<Nbt.Compound>
+    data class Compound(override val elements: kotlin.collections.List<Nbt.Compound>) : Nbt.List<Nbt.Compound>
   }
 
   @Serializable
-  @JvmInline
-  value class Compound(val elements: Map<kotlin.String, Nbt>) : Nbt
+  data class Compound(val elements: Map<kotlin.String, Nbt>) : Nbt
 }
