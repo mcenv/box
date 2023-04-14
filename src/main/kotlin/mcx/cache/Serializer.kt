6e0944ffbe2f7b1345aec71d6213ded27e503571
intsuc <intsuc@gmail.com>
2023-04-14 15:19:57 +0900

    Add version manifest serializers

---
+++ src/main/kotlin/mcx/cache/Serializer.kt
@@ -0,0 +1,102 @@
+@file:UseSerializers(
+  URLSerializer::class,
+  DateSerializer::class,
+)
+
+package mcx.cache
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.UseSerializers
+import java.net.URL
+import java.util.*
+
+@Serializable
+data class VersionManifest(
+  val latest: Latest,
+  val versions: List<Version>,
+) {
+  @Serializable
+  data class Latest(
+    val release: String,
+    val snapshot: String,
+  )
+
+  @Serializable
+  data class Version(
+    val id: String,
+    val type: Type,
+    val url: URL,
+    val time: Date,
+    val releaseTime: Date,
+    val sha1: String,
+  ) {
+    @Serializable
+    enum class Type {
+      @SerialName("release")
+      RELEASE,
+
+      @SerialName("snapshot")
+      SNAPSHOT,
+
+      @SerialName("old_beta")
+      OLD_BETA,
+
+      @SerialName("old_alpha")
+      OLD_ALPHA,
+    }
+  }
+}
+
+@Serializable
+data class Package(
+  val downloads: Downloads,
+  val id: String,
+) {
+  @Serializable
+  data class Downloads(
+    val server: Download,
+    @SerialName("server_mappings") val serverMappings: Download,
+  ) {
+    @Serializable
+    data class Download(
+      val sha1: String,
+      val size: Long,
+      val url: URL,
+    )
+  }
+}
+
+object URLSerializer : KSerializer<URL> {
+  override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("URL", PrimitiveKind.STRING)
+
+  override fun serialize(
+    encoder: Encoder,
+    value: URL,
+  ) {
+    encoder.encodeString(value.toString())
+  }
+
+  override fun deserialize(
+    decoder: Decoder,
+  ): URL {
+    return URL(decoder.decodeString())
+  }
+}
+
+object DateSerializer : KSerializer<Date> {
+  override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Date", PrimitiveKind.STRING)
+
+  override fun serialize(
+    encoder: Encoder,
+    value: Date,
+  ) {
+    encoder.encodeString(value.toInstant().toString())
+  }
+
+  override fun deserialize(
+    decoder: Decoder,
+  ): Date {
+    return Date.from(Instant.parse(decoder.decodeString()))
+  }
+}
