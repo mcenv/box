7bd9144e5a9b0065e712751556b5461470325723
intsuc <intsuc@gmail.com>
2023-05-30 14:48:10 +0900

    Remove unused code

--- src/main/kotlin/mcx/lsp/Markup.kt
+++
@@ -1,8 +0,0 @@
-package mcx.lsp
-
-import org.eclipse.lsp4j.MarkupContent
-import org.eclipse.lsp4j.MarkupKind
-
-fun highlight(text: String): MarkupContent {
-  return MarkupContent(MarkupKind.MARKDOWN, "```mcx\n$text\n```")
-}
