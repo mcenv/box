312b8ea060176280d33ac2f934c4569ce22ebcd9
intsuc <intsuc@gmail.com>
2023-06-08 08:56:27 +0900

    Test mcenv/std
    Closes #366.

--- src/test/kotlin/mcx/pass/BuildTests.kt
+++ src/test/kotlin/mcx/pass/BuildTests.kt
@@ -1,21 +1,23 @@
 package mcx.pass
 
 import kotlinx.coroutines.runBlocking
+import mcx.cache.getOrCreateDependenciesPath
 import mcx.cache.installDependencies
 import mcx.lsp.diagnosticMessage
 import mcx.pass.build.Build
 import java.nio.file.Path
 import kotlin.io.path.Path
+import kotlin.io.path.div
 import kotlin.test.Test
 
 object BuildTests {
   val core: Path = Path("src", "main", "resources", "core")
   val test: Path = Path("src", "test", "resources", "test")
+  val std: Path = getOrCreateDependenciesPath() / "mcenv" / "std-main"
 
   @Test
   fun core() {
     val result = runBlocking {
-      installDependencies(core)
       Build(core, core)()
     }
     result.diagnosticsByPath.forEach { (path, diagnostics) ->
@@ -31,6 +33,20 @@ object BuildTests {
     val result = runBlocking {
       installDependencies(test)
       Build(test, core)()
+    }
+    result.diagnosticsByPath.forEach { (path, diagnostics) ->
+      diagnostics.forEach {
+        println(diagnosticMessage(path, it))
+      }
+    }
+    assert(result.success)
+  }
+
+  @Test
+  fun std() {
+    val result = runBlocking {
+      installDependencies(test)
+      Build(std, core)()
     }
     result.diagnosticsByPath.forEach { (path, diagnostics) ->
       diagnostics.forEach {
